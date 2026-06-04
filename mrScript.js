582e62640c20e7063b3e9a8d16a15df4a3ebabec
Mason Lyons <mdlyons03@gmail.com>
2026-06-04 10:59:04 -0400

    Removing old code

--- mrScript.js
+++ mrScript.js
@@ -1,38 +1,4 @@
-// "use strict";
-
-// var canvas;
-// var gl;
-
-// window.onload = function () {
-//     canvas = document.getElementById("gl-canvas");
-//     gl = canvas.getContext('webgl2');
-//     if (!gl) alert("WebGL 2.0 isn't available");
-//     gl.viewport(0, 0, canvas.width, canvas.height);
-//     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-//     gl.clear(gl.COLOR_BUFFER_BIT);
-// };
-
-// function show(el) {
-//   if (!el) return;
-//   el.classList.remove("hidden");
-// }
-// function hide(el) {
-//   if (!el) return;
-//   el.classList.add("hidden");
-// }
-
-//  /*if (ButtonL) {
-//   ButtonL.addEventListener("click", () => {
-//     hide(movie-list);
-//     hide(recommendations);
-//     show(login-form);
-//     const su = document.getElementById("username");
-//     if (su) su.focus();
-//   });
-// }*/
-
-//  document.getElementById("ButtonL").onclick = function(){show(document.getElementById("login-form"))};
-
+"use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
   const loginToggle = document.getElementById('login-toggle');
